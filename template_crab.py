1cfd36890c6fab77ea8d3e0373755726f2164fd8
cmunozdi <conrado.munoz.diaz@cern.ch>
2025-12-02 11:20:55 +0100

    Preparing reprocessing 2023C v4

--- template_crab.py
+++ template_crab.py
@@ -25,9 +25,9 @@ config.JobType.inputFiles = [
                                 f'{cmssw_base}/src/XConeReclustering/selection_helpers_BoostedTopQuark.so',
                                 f'{cmssw_base}/src/XConeReclustering/selection_helpers_BoostedTopQuark_Dict_rdict.pcm',
                                 f'{cmssw_base}/src/XConeReclustering/selection_helpers_BoostedTopQuark_Dict.cpp',
-                                f'{cmssw_base}/src/XConeReclustering/2023jec/jet_jerc.json.gz',
-                                f'{cmssw_base}/src/XConeReclustering/2023btag/btag_efficiencies_combined.json',
-                                f'{cmssw_base}/src/XConeReclustering/2023btag/btagging.json.gz',
+                                # f'{cmssw_base}/src/XConeReclustering/2023jec/jet_jerc.json.gz',
+                                '/eos/project/r/rtu-topanalysis/cmunozdi/AnalysisSamples_JetTightIDNoLepVeto_Full/output_efficiencies_rdf_topSemi_2023preBPix/btag_efficiencies_combined.json',
+                                # f'{cmssw_base}/src/XConeReclustering/2023btag/btagging.json.gz',
                             ]
 config.JobType.outputFiles = ['_outXConeFileName_']
 
